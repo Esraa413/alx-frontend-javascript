3eadeac1a5e8252bfc95fc71d82db57a883c9b65
Esraa413 <esraaabdelwahab614@gmail.com>
2024-04-25 22:58:41 +0000

    js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,43 +1,16 @@
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
-      throw new Error('Invalid input type');
-    }
-    this._name = name;
-    this._length = length;
-    this._students = students;
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
-  get name() {
-    return this._name;
+  static get [Symbol.species]() {
+    return this;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new Error('Invalid input type');
-    }
-    this._name = value;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  set length(value) {
-    if (typeof value !== 'number') {
-      throw new Error('Invalid input type');
-    }
-    this._length = value;
-  }
-
-  get students() {
-    return this._students;
-  }
-
-  set students(value) {
-    if (!Array.isArray(value)) {
-      throw new Error('Invalid input type');
-    }
-    this._students = value;
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
   }
 }
